fbf9a942e122b93ee1c45de396bc77b2b815b3a4
Tal <tal.bny3@gmail.com>
2020-03-22 16:00:01 +0200

    beautify the entity editor

--- src/components/EntityEditor.js
+++ src/components/EntityEditor.js
@@ -14,21 +14,36 @@ const Styles = styled.div`
 
 class EntityEditor extends React.Component {
     render() {
-      const json= JSON.parse(this.props.jsondata);
-      console.log(json)
-      const items = []
+
+     const json = JSON.parse(this.props.jsondata)
+     const level = parseInt(this.props.level) 
+     
+     const indent = 50 * level
+
+     const items = []
 
       for (var key in json){ 
-        items.push(        
-        <Row>
-            <Col xs lg="1">
-              <Form.Label >{key}</Form.Label>
-            </Col>
-            <Col xs lg="2">
-             <Form.Control type="text" value="" width="10px"/>
-            </Col>
-        </Row>
-        )
+        if (typeof json[key] != 'object') {
+            items.push(        
+                <Row style={{fontSize: 20 , marginLeft: indent}}>
+                    <Col xs lg="1">
+                      <Form.Label >{key}</Form.Label>
+                    </Col>
+                    <Col xs lg="2">
+                     <Form.Control size="sm" type="text" width="20px"/>
+                    </Col>
+                </Row>
+                )
+        } else {
+            items.push(
+                <div>
+                
+                    <Form.Label style={{fontSize: 20, marginLeft: indent}} >{key}</Form.Label>
+                
+                    <EntityEditor level={level+1} jsondata={JSON.stringify(json[key])}></EntityEditor>
+                </div>
+            )
+        }
       }
 
       return (
