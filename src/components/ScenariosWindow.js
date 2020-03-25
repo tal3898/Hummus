935da2907b51265c3bc51849744335493b1602cf
Tal <tal.bny3@gmail.com>
2020-03-25 22:47:58 +0200

    beautify the scenarios window

--- src/components/ScenariosWindow.js
+++ src/components/ScenariosWindow.js
@@ -1,70 +1,94 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Form, Col, Row } from 'react-bootstrap';
 
 const Styles = styled.div`
-  .headline {
-    font-size: 3.0em;
-    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-    background-color: #1e88e5;
-    box-shadow: 1px 5px 5px #666666;
-    
-    border-radius: 20px;
+
+
+.all {
+  margin-top:20px
 }
 
-  .main-comp {
-      width: 400px;
-      height: 700px;
-      background: #fafafa;
-      border-radius: 20px;
-      border-color: black;
-      border-width: medium;
-      margin: 20px;
+.headline {
+  font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+  color: #bbdefb;
 
-      border-width:1px;
-      border-style:outset;
+}
+
+.w3-card-4 {
+  height:600px;
+}
+
+  .action {
+    margin-top:5px;
   }
 
-  .item {
-    height: 40px;
-    align: right;
-    font-size: 20px;
-    padding-left: 15px;
+  .scenario-name {
+    font-size: 1.7em;
+    width:300px;
+    
+    float:right;
+    margin-right: 10px;
+  }
+
+  .field {
+    height: 40px; 
+    float:right;
+    margin-top : 5px;
     &:hover { background: #bbdefb; }
-  }
+}
 
 `;
 export const ScenariosWindow = () => (
   <Styles>
 
-    <div  className="w3-container">
-
-      <div className="w3-card-4" style={{ width: 400 }}>
-        <header dir="rtl" class="w3-container w3-blue">
-          <h1>תרחישים</h1>
-        </header>
+    <div className="all w3-card-4" style={{ width: 400 }}>
+      <header dir="rtl" class="w3-container w3-blue">
+        <h1 className="headline">תרחישים</h1>
+      </header>
 
 
-        <div className="w3-container">
+      <div className="w3-container">
 
-          <div dir="rtl" className='scenarios-list'>
-            <div className='item'>
-            <i class="far fa-folder-open"></i>
-            
-              a
-            </div>
-            <div className='item'>
-            <i class="far fa-folder-open"></i>
-              a
-            </div>
-            <div className='item'>
-            <i class="fas fa-file-invoice"></i>
-              a
-            </div>
-          </div>
+        <div dir="rtl" className='scenarios-list'>
+          <Row className="field">
+            <Col lg="1" className="col-md-2">
+              <i class="action far fa-folder-open fa-2x"></i>
+            </Col>
+            <Col className="col-md-2">
+              <span style={{ float: 'right' }} className="scenario-name">טל</span>
+            </Col>
+          </Row>
+
+          <Row className="field">
+            <Col lg="1" className="col-md-2">
+              <i class="action far fa-folder-open fa-2x"></i>
+            </Col>
+            <Col className="col-md-2">
+              <span dir="rtl" className="scenario-name">שחר</span>
+            </Col>
+          </Row>
+
+          <Row className="field">
+            <Col lg="1" className="col-md-2">
+              <center>
+                <i class="action far fa-file fa-2x"></i>
+              </center>
+            </Col>
+            <Col className="col-md-2">
+              <span className="scenario-name">כתיבה רגילה</span>
+            </Col>
+          </Row>
 
         </div>
 
       </div>
+
     </div>
+
+
+
+
+
   </Styles>
 )
