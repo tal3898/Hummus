9152ce28b962ef275f721217bd338ea8b1c32c9d
Tal <tal.bny3@gmail.com>
2020-03-25 16:56:12 +0200

    added logo

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import { Nav, Navbar, Form, FormControl, Col, Row } from 'react-bootstrap';
 import styled from 'styled-components';
-const Styles = styled.div`
-  .navbar { background-color: #1e88e5; }
+import Logo from './logo.png'
 
-  .navbar-brand {
-    font-size: 3.0em;
+
+const Styles = styled.div`
+  .navbar { 
+    background-color: #1e88e5; 
+    height: 90px;
+  }
+
+  .headline {
+    font-size: 3.5em;
     color: #bbdefb;
     font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
     &:hover { color: #bbdefb; }
   }
 
+  .logo {
+    margin-left:10px;
+    width:70px;
+    height:70px;
+  }
+
 `;
 export const NavigationBar = () => (
   <Styles>
-    <Navbar dir='ltr' expand="lg">
-      <Navbar.Brand href="/">Hummus</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    </Navbar>
+    <Row className="navbar">
+      <Col lg="0">
+        <img className="logo" src={Logo} />
+      </Col>
+      <Col >
+        <span className="headline">Hummus</span>
+      </Col>
+
+    </Row>
   </Styles>
 )
