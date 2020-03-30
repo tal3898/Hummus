5ec0921a5fd633ece9d2a374ba3c73698a53d757
Tal <tal.bny3@gmail.com>
2020-03-30 14:41:55 +0300

    added connection to db, and when app is going up, it reqeust for the scenario list

--- server.js
+++ server.js
@@ -1,42 +1,39 @@
 const express = require('express');
+const mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+const dbUrl = "mongodb://localhost:27017/";
 
 var bodyParser = require('body-parser')
 
-
 const app = express();
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+	extended: true
+}));
+
+
+
 
 const port = 5000;
 
-app.post('/scenario', (req,res) => {
+app.post('/scenario', (req, res) => {
 	console.log('in post, with body ' + JSON.stringify(req.body));
 
-	if (req.body.path == '/') {
-		res.json([
-			{
-				name: 'first',
-				files: []
-			}, {
-				name: 'second',
-				files: []
-			}, {
-				name: 'third',
-				steps: []
-			}
+	MongoClient.connect(dbUrl, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("HummusDB");
+		dbo.collection("scenario").findOne({}, function(err, result) {
+		  if (err) throw err;
+		  res.json([
+			result
 		]);
-	} else {
-		res.json([
-			{
-				name: 'inner file',
-				files: []
-			}
-		]);
-	}
+		  db.close();
+		});
+	  });
+
+
 
 });
 
-app.listen(port, ()=> console.log('server started on port ' + port) );
+app.listen(port, () => console.log('server started on port ' + port));
