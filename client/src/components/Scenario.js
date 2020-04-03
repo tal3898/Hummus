eafd67667880729f22cea92c9055985bb111670c
Tal <tal.bny3@gmail.com>
2020-04-03 16:50:36 +0300

    added the function getStepNgRequest to Scenario component, which gets a step number, and return its full request json

--- client/src/components/Scenario.js
+++ client/src/components/Scenario.js
@@ -11,7 +11,6 @@ import Popup from "reactjs-popup";
 
 import ActionMap from '../globals/ActionMap.json'
 
-
 const Styles = styled.div`
 .main-comp {
     padding-right: 50px;
@@ -94,7 +93,11 @@ class Scenario extends React.Component {
     }
 
     openJsonPopup() {
+        var aaa = this.convertJsonTemplateToActualJson(JSON.parse(this.context.data.currScenario.steps[0].jsonToEdit));
+
         this.state.json = this.ngRequestEditorRef.current.getFullRequestJson();
+
+
         this.state.isJsonPopupOpen = true;
         this.setState(this.state);
     }
@@ -111,9 +114,85 @@ class Scenario extends React.Component {
         this.state.scenarioData.steps = this.context.data.currScenario.steps;
     }
 
+    /**
+     * The method gets a json that the keys format are with |..|..|, and values or dynamic, like [GEN], or [NOW], or enum.
+     * so the method gets the json, and convert it to a normal json, when keys are only the name of the keys,
+     * and values or normal values.
+     * @param {*} json 
+     */
+    convertJsonTemplateToActualJson(json) {
+        var resultJson = {};
+
+        // loop on all value fields
+        for (var key in json) {
+            if (typeof json[key] != 'object') {
+                var keyName = key.split('|')[0];
+                var keyValue = this.getFieldFinalValue(key, json[key]);
+                
+                resultJson[keyName] = keyValue;
+            }
+        }
+
+        // loop on all object fileds
+        for (var key in json) {
+            var keyName = key.split('|')[0];
+
+            // arrays
+            if (typeof json[key] == 'object' && Array.isArray(json[key]) ) {
+                resultJson[keyName] = []
+                for (var index in json[key]) {
+                    var singleJsonResult = this.convertJsonTemplateToActualJson(json[key][index]);
+                    resultJson[keyName].push(singleJsonResult);
+                }
+            } else if (typeof json[key] == 'object') {
+                resultJson[keyName] = this.convertJsonTemplateToActualJson(json[key]);
+            }
+        }
+
+        return resultJson;
+    }
+
+    getFieldFinalValue(key, fieldValue) {
+        var finalValue = fieldValue;
+        var fieldType = key.split('|')[1];
+
+        if (fieldValue == '[NOW]') {
+            finalValue = new Date().toISOString();;
+        } else if (fieldValue == '[GEN]') {
+            var randomString = "";
+            for (let step = 0; step < 5; step++) {
+                var randomLetter = "abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 1000) % 26];
+                randomString += randomLetter
+            }
+
+            finalValue = randomString
+        } else if (fieldType == "number" || fieldType == "enum") {
+            finalValue = parseInt(fieldValue); // If enum, and looks like this : "50 - ABC", it will parse only the 50 to int
+        } else if (fieldType == "float") {
+            finalValue = parseFloat(fieldValue);
+        }
+
+        return finalValue;
+    }
+
+    getStepNgRequest(stepNumber) {
+        var entityJson  = this.convertJsonTemplateToActualJson( JSON.parse(this.context.data.currScenario.steps[stepNumber].jsonToEdit) );
+
+        var fullRequestJson = {
+            "Entity": this.context.data.currScenario.steps[stepNumber].entity,
+            "SendingTime": new Date().toISOString(),
+            "Reality": this.context.data.currScenario.steps[stepNumber].reality,
+            "Version": this.context.data.currScenario.steps[stepNumber].version,
+            "System": this.context.data.currScenario.steps[stepNumber].system,
+            "Entities": [entityJson]
+        }
+
+        return fullRequestJson;
+    }
 
     sendJsonToNG() {
-        var fullRequestJson = this.ngRequestEditorRef.current.getFullRequestJson();
+        var a = this.getStepNgRequest(0);
+
 
         var bodyJ = JSON.stringify({
             nameA: "paul rudd",
@@ -145,8 +224,6 @@ class Scenario extends React.Component {
                 toast.error("Error sending write request", toastProperties);
                 console.error("NG error: ", error)
             });
-
-        console.log(JSON.stringify(fullRequestJson));
     }
 
     close() {
