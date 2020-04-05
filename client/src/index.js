774f4684711821ff85a39085abc5a96f7d9db8ee
Tal <tal.bny3@gmail.com>
2020-04-05 15:58:29 +0300

    fixed exception thrown when click on a shortcut

--- client/src/index.js
+++ client/src/index.js
@@ -12,38 +12,40 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 document.onkeyup = function (e) {
-    if (e.ctrlKey && e.keyCode == 8) {
-        // ctrl + back
-        document.getElementById("goBackBtn").click();
-    } if (e.ctrlKey && e.keyCode == 66) {
-        // ctrl + b
-        document.getElementById("expandAllBtn").click();
-    } else if (e.ctrlKey && e.which == 81) {
-        // ctrl + q
-        document.getElementById("scenariosListBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 68) {
-        // ctrl + alt + d
-        document.getElementById("sendStepBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 79) {
-        // ctrl + alt + o
-        document.getElementById("openSavePopupBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 83) {
-        // ctrl + alt + s
-        document.getElementById("saveScenarioBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 83) {
-        // ctrl + alt + o
-        document.getElementById("saveScenarioBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 78) {
-        // ctrl + alt + n
-        document.getElementById("addStepBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 82) {
-        // ctrl + alt + r
-        document.getElementById("removeStepBtn").click();
-    }  else if (e.ctrlKey && e.altKey && e.which == 74) {
-        // ctrl + alt + j
-        document.getElementById("showJsonBtn").click();
-    } else if (e.ctrlKey && e.altKey && e.which == 67) {
-        // ctrl + alt + c
-        document.getElementById("copyJsonBtn").click();
-    } 
+    try {
+        if (e.ctrlKey && e.keyCode == 8) {
+            // ctrl + back
+            document.getElementById("goBackBtn").click();
+        } if (e.ctrlKey && e.keyCode == 66) {
+            // ctrl + b
+            document.getElementById("expandAllBtn").click();
+        } else if (e.ctrlKey && e.which == 81) {
+            // ctrl + q
+            document.getElementById("scenariosListBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 68) {
+            // ctrl + alt + d
+            document.getElementById("sendStepBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 79) {
+            // ctrl + alt + o
+            document.getElementById("openSavePopupBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 83) {
+            // ctrl + alt + s
+            document.getElementById("saveScenarioBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 83) {
+            // ctrl + alt + o
+            document.getElementById("saveScenarioBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 78) {
+            // ctrl + alt + n
+            document.getElementById("addStepBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 82) {
+            // ctrl + alt + r
+            document.getElementById("removeStepBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 74) {
+            // ctrl + alt + j
+            document.getElementById("showJsonBtn").click();
+        } else if (e.ctrlKey && e.altKey && e.which == 67) {
+            // ctrl + alt + c
+            document.getElementById("copyJsonBtn").click();
+        }
+    } catch (err) { }
 };
