d41fdb2bbc7b0baec5025c291d5d240854ad137c
Tal <tal.bny3@gmail.com>
2020-04-13 00:02:29 +0300

    fixed the linking bug #41, for number 3,4,5 but with disable fields

--- client/src/components/Scenario.js
+++ client/src/components/Scenario.js
@@ -138,7 +138,7 @@ class Scenario extends React.Component {
     getStepNgRequest(stepNumber) {
         var currStepJson = JSON.parse(this.context.data.currScenario.steps[stepNumber].jsonToEdit);
 
-        var disabledFields = this.context.data.currScenario.steps[this.context.data.currOpenStep].disabledFields;
+        var disabledFields = this.context.data.currScenario.steps[stepNumber].disabledFields;
         var entityJson = convertJsonTemplateToActualJson(currStepJson, disabledFields);
 
         var fullRequestJson = {
