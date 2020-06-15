78a2dcc584c89343fc5d412358aa6a41b28acb5f
Tal <tal.bny3@gmail.com>
2020-06-15 21:04:47 +0300

    resolved issue #102

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ var NgUrl = process.env.NG_URL;
 
 const app = express();
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json({limit: '5mb'}));       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	limit: '5mb',
 	extended: true
 }));
 
