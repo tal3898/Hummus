076d2f6b218f6fdc31ddfa0ca9ef6588b796d37e
Tal <tal.bny3@gmail.com>
2020-07-10 18:13:22 +0300

    added a script, that runs the hummus on new chrome, with new data dir, with no cors, so we can send requests to localhost

---
+++ hummus.bat
@@ -0,0 +1,3 @@
+cd %userprofile%
+mkdir hummus-chrome-data
+"C:\Program Files (x86)\Google\Chrome\Application\chrome" --user-data-dir="%userprofile%\hummus-chrome-data" --disable-web-security localhost:3000
