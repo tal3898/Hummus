2cc1ea7c292fc183b0b56a7dd3b7524f8abb04e4
tal <tal.bny3@gmail.com>
2020-08-12 12:33:00 +0300

    fixed the disable field, by removing the id property in FlipSwitch

--- src/components/FlipSwitch/FlipSwitch.js
+++ src/components/FlipSwitch/FlipSwitch.js
@@ -7,13 +7,14 @@ function FlipSwitch(props) {
     return (
         <div class="flipswitch" onClick={props.onClick} style={{marginTop:5}}>
 
-            <input type="checkbox" name="flipswitch" class="flipswitch-cb" id="fs"
+            <input type="checkbox" name="flipswitch" class="flipswitch-cb" 
                 checked={props.checked} />
             <label class="flipswitch-label" for="fs">
                 <div class="flipswitch-inner"></div>
                 <div class="flipswitch-switch"></div>
             </label>
         </div>
+
     );
 }
 
