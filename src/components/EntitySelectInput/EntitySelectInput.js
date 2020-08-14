d0a2a59cc161ed302ca3af47c9262a5f677c3650
Tal <tal.bny3@gmail.com>
2020-08-15 00:40:16 +0300

    added a first entity select input, for the linking entities popup

---
+++ src/components/EntitySelectInput/EntitySelectInput.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Main = styled("div")`
+  font-family: sans-serif;
+  background: #f0f0f0;
+  height: 100vh;
+`;
+
+const DropDownContainer = styled("div")`
+  width: 13.5em;
+  margin: 0 auto;
+`;
+
+const DropDownHeader = styled("div")`
+  margin-bottom: 0.8em;
+  padding: 0.4em 2em 0.4em 1em;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  font-weight: 500;
+  font-size: 1.3rem;
+  color: #3faffa;
+  background: #ffffff;
+`;
+
+const DropDownListContainer = styled("div")``;
+
+const DropDownList = styled("ul")`
+  padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: #3faffa;
+  font-size: 1.2rem;
+  font-weight: 500;
+  &:first-child {
+    padding-top: 0.8em;
+  }
+`;
+
+const ListItem = styled("div")`
+  list-style: none;
+  margin-bottom: 0.4em;
+  margin-left: 0.5em;
+`;
+
+const ListItemTitle = styled("li")`
+  list-style: none;
+  font-weight: 800;
+  margin-bottom: 0.5em;
+`;
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [numberOfSelectedEntities, setNumberOfSelectedEntities] = useState(0);
+  const [checkedEntities, setCheckedEntities] = useState({
+    "step 1": [false, false],
+    "step 2": [false]
+  });
+
+  const inputJson = {
+    "step 1": ["obj", "obj"],
+    "step 2": ["obj"]
+  };
+
+  const numberOfEntitiesToSelect = 2;
+
+  const toggling = () => setIsOpen(!isOpen);
+
+  const checklistClicked = (event, step, index) => {
+    checkedEntities[step][index] = event.target.checked;
+    setCheckedEntities(checkedEntities);
+
+    if (event.target.checked) {
+      setNumberOfSelectedEntities(numberOfSelectedEntities + 1);
+    } else {
+      setNumberOfSelectedEntities(numberOfSelectedEntities - 1);
+    }
+  };
+
+  return (
+    <Main>
+      <h1>Custom Select/dropdown aa</h1>
+
+      <DropDownContainer>
+        <DropDownHeader onClick={toggling}>select objectives</DropDownHeader>
+        {isOpen && (
+          <DropDownListContainer>
+            <DropDownList>
+              {Object.keys(inputJson).map((key) => (
+                <div>
+                  <ListItemTitle>{key}</ListItemTitle>
+                  {inputJson[key].map((value, index) => (
+                    <ListItem>
+                      <input
+                        style={{ display: "inline-block" }}
+                        type="checkbox"
+                        id="vehicle1"
+                        name="vehicle1"
+                        value="Bike"
+                        checked={checkedEntities[key][index]}
+                        disabled={
+                          numberOfSelectedEntities ===
+                            numberOfEntitiesToSelect &&
+                          !checkedEntities[key][index]
+                        }
+                        onChange={(e) => checklistClicked(e, key, index)}
+                      />
+                      <span>{value}</span>
+                    </ListItem>
+                  ))}
+                </div>
+              ))}
+            </DropDownList>
+          </DropDownListContainer>
+        )}
+      </DropDownContainer>
+    </Main>
+  );
+}
