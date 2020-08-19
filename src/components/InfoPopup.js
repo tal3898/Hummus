251a205008c02fcaf6790a80b2e2c3d63c156031
Tal <tal.bny3@gmail.com>
2020-08-19 20:19:17 +0300

    updated info popup

--- src/components/InfoPopup.js
+++ src/components/InfoPopup.js
@@ -67,7 +67,7 @@ class InfoPopup extends React.Component {
                                 <li>כל הפיצרים של החומוס</li>
                                 <li>איך עושים כתיבה היררכית</li>
                                 <li>שמירת תרחישים</li>
-                                <li>מה זה "האם התקן פעיל" ?</li>
+                                <li>מה זה "האם טלטלה פעיל" ?</li>
                                 <li>איך כותבים לlocalhost</li>
                             </ul>
                             <hr/>
