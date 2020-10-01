3b35bba8e3df8e2dee85bbc8ec327b53b3cc0fc4
tal <tal.bny3@gmail.com>
2020-10-01 14:27:05 +0300

    added ErrorBoundary, used it in linking fields. cute, but not very useful here

--- src/components/LinkingFieldsPopup.js
+++ src/components/LinkingFieldsPopup.js
@@ -9,7 +9,7 @@ import JsonViewer from './JsonViewer';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { convertJsonTemplateToActualJson } from './Utility'
-
+import ErrorBoundary from './ErrorBoundary'
 import LinksVisualization from './LinksVisualization'
 import LinkEntity from './LinkingEntities/LinkEntity'
 
@@ -161,116 +161,119 @@ class LinkingFieldsPopup extends React.Component {
                     closeOnDocumentClick
                 >
 
-                    <div style={{ height: '600px', overflowY: 'scroll' }}>
+                    <ErrorBoundary>
+                        <div style={{ height: '600px', overflowY: 'scroll' }}>
 
-                        <Tabs
-                            id="controlled-tab-example"
-                            style={{ position: 'absolute', width: '99%', marginLeft: 0, zIndex: 100, background: 'white' }}
-                            activeKey={this.state.tab}
-                            onSelect={(k) => { this.state.tab = k; this.setState(this.state) }}
+                            <Tabs
+                                id="controlled-tab-example"
+                                style={{ position: 'absolute', width: '99%', marginLeft: 0, zIndex: 100, background: 'white' }}
+                                activeKey={this.state.tab}
+                                onSelect={(k) => { this.state.tab = k; this.setState(this.state) }}
 
-                        >
-                            <Tab eventKey="ישויות" dir="rtl" title="ישויות">
-                                <LinkEntity
-                                    closePopupCallback={() => this.close()}
-                                />
-                            </Tab>
-                            <Tab eventKey="שדות" title="שדות">
-                                <div style={{ marginTop: 60, width: '100%', overflowX: 'hidden' }}>
-                                    <div style={{ marginBottom: 30 }}>
-                                        <i onClick={() => this.addLink()} className="fas fa-check link-check fa-2x"></i>
-                                        <center>
-                                            <Form.Label style={{ fontSize: 30, marginBottom: 1 }}>קישור שדות</Form.Label>
-                                        </center>
-                                    </div>
-
-                                    <div style={{ marginRight: 25 }}>
-                                        <div style={{ float: 'right' }}>
-                                            כאן תוכלו לקשר שדות בין צעדים בתוך התרחיש <br />
+                            >
+                                <Tab eventKey="ישויות" dir="rtl" title="ישויות">
+                                    <LinkEntity
+                                        closePopupCallback={() => this.close()}
+                                    />
+                                </Tab>
+                                <Tab eventKey="שדות" title="שדות">
+                                    <div style={{ marginTop: 60, width: '100%', overflowX: 'hidden' }}>
+                                        <div style={{ marginBottom: 30 }}>
+                                            <i onClick={() => this.addLink()} className="fas fa-check link-check fa-2x"></i>
+                                            <center>
+                                                <Form.Label style={{ fontSize: 30, marginBottom: 1 }}>קישור שדות</Form.Label>
+                                            </center>
                                         </div>
-                                        <br />
-                                        <div style={{ float: 'right' }}>
-                                            תבחרו את הצעד והשדה ממנו אתה רוצה להעתיק את הערך, ותבחרו את השדה אליו אתם רוצים להעתיק <br />
+
+                                        <div style={{ marginRight: 25 }}>
+                                            <div style={{ float: 'right' }}>
+                                                כאן תוכלו לקשר שדות בין צעדים בתוך התרחיש <br />
+                                            </div>
+                                            <br />
+                                            <div style={{ float: 'right' }}>
+                                                תבחרו את הצעד והשדה ממנו אתה רוצה להעתיק את הערך, ותבחרו את השדה אליו אתם רוצים להעתיק <br />
+                                            </div>
+                                            <br />
+                                            <br />
                                         </div>
-                                        <br />
-                                        <br />
+
+                                        <Row>
+                                            <Col lg="6" style={{ marginRight: 0, paddingRight: 0 }}>
+
+                                                <div style={{ marginRight: 30, marginBottom: 10 }}>
+                                                    <Form.Label style={{ float: 'right', marginBottom: 1, marginLeft: 20, fontSize: 20 }}>בחר צעד מקור</Form.Label>
+                                                    <Form.Control
+                                                        style={{ width: 150, float: 'right', marginTop: 2 }}
+                                                        size="sm"
+                                                        value={this.selectedScenarioNumber + '-' + this.context.data.currScenario.steps[this.selectedScenarioNumber].name}
+                                                        onChange={(event) => this.loadSourceJson(event)}
+                                                        as="select">
+                                                        {this.getStepsOptions()}
+                                                    </Form.Control>
+                                                    <br />
+                                                </div>
+
+                                                <div style={{ marginTop: 25 }}>
+                                                    <div style={{ float: 'right', marginRight: 30 }} dir="rtl">בחר שדה מקור</div>
+                                                    <br /><br />
+                                                    <div style={{ marginRight: 10, marginLeft: 10, height: 400 }} className="directory-tree">
+                                                        <JsonViewer
+                                                            onKeySelected={(event) => this.selectedOriginPath = event}
+                                                            type="json"
+                                                            json={this.state.fromJson}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </Col>
+
+
+                                            <Col lg="6" style={{ marginLeft: 0, paddingLeft: 0, paddingRight: 0 }}>
+                                                <div style={{ marginRight: 30, marginBottom: 10 }}>
+                                                    <Form.Label style={{ float: 'right', marginBottom: 1, marginLeft: 20, fontSize: 20 }}>צעד יעד</Form.Label>
+                                                    <Form.Control
+                                                        style={{ width: 150, float: 'right', marginTop: 2 }}
+                                                        size="sm"
+                                                        disabled
+                                                        onChange={(event) => this.loadSourceJson(event)}
+                                                        as="select">
+                                                        <option>{this.state.stepNumber + ' - ' +
+                                                            this.context.data.currScenario.steps[this.state.stepNumber].name}</option>
+                                                    </Form.Control>
+                                                    <br />
+                                                </div>
+
+
+                                                <div style={{ marginTop: 25 }}>
+                                                    <div style={{ float: 'right', marginRight: 30 }} dir="rtl">בחר שדה יעד</div>
+                                                    <br /><br />
+                                                    {/** TODO put the div with the style, and the css of the class , inside the JsonViewer */}
+                                                    <div style={{ marginRight: 0, marginLeft: 0, height: 400 }} className="directory-tree">
+
+                                                        <JsonViewer
+                                                            onKeySelected={(event) => this.selectedDestPath = event}
+                                                            type="json"
+                                                            json={this.state.json}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                        </Row>
                                     </div>
-
-                                    <Row>
-                                        <Col lg="6" style={{ marginRight: 0, paddingRight: 0 }}>
-
-                                            <div style={{ marginRight: 30, marginBottom: 10 }}>
-                                                <Form.Label style={{ float: 'right', marginBottom: 1, marginLeft: 20, fontSize: 20 }}>בחר צעד מקור</Form.Label>
-                                                <Form.Control
-                                                    style={{ width: 150, float: 'right', marginTop: 2 }}
-                                                    size="sm"
-                                                    value={this.selectedScenarioNumber + '-' + this.context.data.currScenario.steps[this.selectedScenarioNumber].name}
-                                                    onChange={(event) => this.loadSourceJson(event)}
-                                                    as="select">
-                                                    {this.getStepsOptions()}
-                                                </Form.Control>
-                                                <br />
-                                            </div>
-
-                                            <div style={{ marginTop: 25 }}>
-                                                <div style={{ float: 'right', marginRight: 30 }} dir="rtl">בחר שדה מקור</div>
-                                                <br /><br />
-                                                <div style={{ marginRight: 10, marginLeft: 10, height: 400 }} className="directory-tree">
-                                                    <JsonViewer
-                                                        onKeySelected={(event) => this.selectedOriginPath = event}
-                                                        type="json"
-                                                        json={this.state.fromJson}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </Col>
+                                </Tab>
+                                <Tab eventKey="ויזואליזציה" title="ויזואליזציה">
+                                    <div style={{ marginTop: 60, marginLeft: 0, fontSize: 20 }}>
+                                        <LinksVisualization />
+                                    </div>
+                                </Tab>
+                            </Tabs>
 
 
-                                        <Col lg="6" style={{ marginLeft: 0, paddingLeft: 0, paddingRight: 0 }}>
-                                            <div style={{ marginRight: 30, marginBottom: 10 }}>
-                                                <Form.Label style={{ float: 'right', marginBottom: 1, marginLeft: 20, fontSize: 20 }}>צעד יעד</Form.Label>
-                                                <Form.Control
-                                                    style={{ width: 150, float: 'right', marginTop: 2 }}
-                                                    size="sm"
-                                                    disabled
-                                                    onChange={(event) => this.loadSourceJson(event)}
-                                                    as="select">
-                                                    <option>{this.state.stepNumber + ' - ' +
-                                                        this.context.data.currScenario.steps[this.state.stepNumber].name}</option>
-                                                </Form.Control>
-                                                <br />
-                                            </div>
-
-
-                                            <div style={{ marginTop: 25 }}>
-                                                <div style={{ float: 'right', marginRight: 30 }} dir="rtl">בחר שדה יעד</div>
-                                                <br /><br />
-                                                {/** TODO put the div with the style, and the css of the class , inside the JsonViewer */}
-                                                <div style={{ marginRight: 0, marginLeft: 0, height: 400 }} className="directory-tree">
-
-                                                    <JsonViewer
-                                                        onKeySelected={(event) => this.selectedDestPath = event}
-                                                        type="json"
-                                                        json={this.state.json}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </div>
-                            </Tab>
-                            <Tab eventKey="ויזואליזציה" title="ויזואליזציה">
-                                <div style={{ marginTop: 60, marginLeft: 0, fontSize: 20 }}>
-                                    <LinksVisualization />
-                                </div>
-                            </Tab>
-                        </Tabs>
-
-
-                    </div>
+                        </div>
+                    </ErrorBoundary>
                 </Popup>
 
             </Styles>
+
         )
     }
 }
